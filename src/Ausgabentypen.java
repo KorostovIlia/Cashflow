cf5e5541bf377c936f614297602fc17a51b5ccf1
JulianSoe <j.soe@gmx.de>
2023-10-13 14:39:08 +0200

    #2 interface Ausgabetypen gelöscht

--- src/Ausgabentypen.java
+++
@@ -1,7 +0,0 @@
-public class Ausgabentypen {
-    private String name;
-    private double amount;
-
-    public Ausgabentypen(String name, double amount) {
-    }
-}
---
+++ src/variableAusgaben.java
@@ -0,0 +1,7 @@
+public class variableAusgaben {
+    private String name;
+    private double amount;
+
+    public variableAusgaben(String name, double amount) {
+    }
+}
