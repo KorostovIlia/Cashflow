4161a0462dd8764e8ba124e39760d6cab41c6119
Ilia_Korostov <ilia.korostov@gmail.com>
2023-10-13 16:04:30 +0200

    add new test for BalanceMinus

---
+++ test/BalanceMinus.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BalanceMinus {
+
+    @Test
+    public void changeBudget() {
+        Balance balance = new Balance(100);
+        VariableExpense banana = new VariableExpense("banana", 1);
+        Assertions.assertEquals(99, balance.getBalance(), "You didn't buy a banana");
+    }
+}
